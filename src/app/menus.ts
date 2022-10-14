48a937031a282e61d8035d136db08a24636428d7
David Nguyen <davidnguyen030499@gmail.com>
2022-10-15 00:30:20 +0200

    update

--- src/app/menus.ts
+++ src/app/menus.ts
@@ -9,35 +9,35 @@ export const menus = [
   {
     name: "Pizza Capricciosa",
     img: "https://www.italianstylecooking.net/wp-content/uploads/2022/01/Pizza-capricciosa.jpg",
-    description: "tomato sauce, cheese, spicy salami, black olives, basil, olive oil",
+    description: "tomato sauce, ham, mushrooms, mozzarella & srtichokes",
     price: 15.50,
     availability: true
   },
   {
     name: "Pizza Margerita",
     img: "https://cdn.shopify.com/s/files/1/0410/4598/3397/articles/Margherita-9920.jpg?crop=center&height=800&v=1644589966&width=800",
-    description: "Some quick example text to build on the card title nd make up the bulk of the cards content",
+    description: "tomato sauce, olive oil, mozzarella & basil",
     price: 10.70,
     availability: false
   },
   {
     name: "Pizza Prosciutto",
     img: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/48fcca95-c468-4a3d-bd48-d2240ec65a05/Derivates/3871b6f6-b560-40f9-af63-104d91e10db3.jpg",
-    description: "Some quick example text to build on the card title nd make up the bulk of the cards content",
+    description: "tomato sauce, olive oil, cheese, prosciutto & arugula",
     price: 15.50,
     availability: true
   },
   {
-    name: "Pizza Capricciosa",
-    img: "https://www.italianstylecooking.net/wp-content/uploads/2022/01/Pizza-capricciosa.jpg",
-    description: "Some quick example text to build on the card title nd make up the bulk of the cards content",
-    price: 15.50,
+    name: "Ricotta ravioli",
+    img: "https://www.pastaweb.de/wp-content/uploads/2006/04/ricotta-spinat-fuellung.jpg",
+    description: "ricotta ravioli, parmesan, basil",
+    price: 11.50,
     availability: false
   },
   {
     name: "Spaghetti Carbonara",
     img: "https://images.lecker.de/,id=94f68b36,b=lecker,w=610,cg=c.jpg",
-    description: "Some quick example text to build on the card title nd make up the bulk of the cards content",
+    description: "spaghetti, pancetta, cheese, olive oil",
     price: 11.50,
     availability: true
   }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  logo : string = "https://www.osteriadegliamici.com/images/ristorante/RISTORANTE350-min.png";
+  // logo : string = "https://www.osteriadegliamici.com/images/ristorante/RISTORANTE350-min.png";
+  logo : string = "https://freepngclipart.com/thumb/chef/78186-thumb-signal-cooking-chef-vector-graphics-thumb.png";
   constructor() { }
 
   ngOnInit(): void {
